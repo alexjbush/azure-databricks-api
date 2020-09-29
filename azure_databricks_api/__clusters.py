072741134012dd02267dd9ab9035b19de11b52ef
Alex Bush <6716812+alexjbush@users.noreply.github.com>
2020-09-29 17:41:53 +1300

    Fix dictionary type check in create

--- azure_databricks_api/__clusters.py
+++ azure_databricks_api/__clusters.py
@@ -82,7 +82,7 @@ class ClusterAPI(RESTBase):
             kwargs['autotermination_minutes'] = autotermination_minutes
 
         # Specify the size of the cluster
-        if type(num_workers) == 'dict':
+        if type(num_workers) is dict:
             cluster_config['autoscale'] = num_workers
         else:
             cluster_config['num_workers'] = int(num_workers)
